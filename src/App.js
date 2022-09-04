7c261a2e0ce5dcf947f9eb0050ffd9999cfb6435
darian <darian.alvarez.t@gmail.com>
2022-09-04 03:34:01 -0400

    add code base and dependencies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+
+import GatewayList from './modules/gateway/pages/GatewayList';
+
+function App() {
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <GatewayList/>
+    </Box>
+  );
+}
+
+export default App;
